1b6e18be35f7e655993f9836fb9b6ecc62996b39
mikeyhalla <michaelj.hall@outlook.com>
2017-11-27 14:42:26 -0600

    Update

--- Maintenance/Program.cs
+++ Maintenance/Program.cs
@@ -172,6 +172,7 @@ namespace Maintenance
                 if (ConfigurationManager.GetSection("PathFilesToDelete") as NameValueCollection != null)
                 {
                     int deleteTemp = 0;
+                    Retry:;
                     foreach (var paths in ConfigurationManager.GetSection("PathFilesToDelete") as NameValueCollection)
                     {
                         try
@@ -209,6 +210,38 @@ namespace Maintenance
                                         catch (IOException)
                                         {
                                             continue;
+                                        }
+                                        catch (Exception)
+                                        {
+                                            foreach (var catcher in ConfigurationManager.GetSection("PathFilesToDeleteCatch") as NameValueCollection)
+                                            {
+                                                try
+                                                {
+                                                    string catcherVariable = (ConfigurationManager.GetSection("PathFilesToDeleteCatch") as NameValueCollection).GetValues(catcher.ToString()).FirstOrDefault();
+
+                                                    var catcherPath = Environment.ExpandEnvironmentVariables(catcherVariable);
+                                                    if (catcherPath == filesPath)
+                                                    {
+                                                        DialogResult result = MessageBox.Show(filesPath + " cannot be deleted at this time. Please check that a process is not holding it and then try again.", "Maintenance", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Exclamation);
+                                                        if (result == DialogResult.Abort)
+                                                        {
+                                                            Environment.Exit(0);
+                                                        }
+                                                        else if (result == DialogResult.Retry)
+                                                        {
+                                                            goto Retry;
+                                                        }
+                                                        else if (result == DialogResult.Ignore)
+                                                        {
+                                                            continue;
+                                                        }
+                                                    }
+                                                }
+                                                catch(Exception)
+                                                {
+                                                    continue;
+                                                }
+                                            }
                                         }
                                     }
                                 }
